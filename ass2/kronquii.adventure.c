1cde75f89b918e6ee32181455c59c6a937edac23
Ian Kronquist <iankronquist@teleport.com>
2015-07-19 21:42:39 -0700

    Working version

--- ass2/kronquii.adventure.c
+++ ass2/kronquii.adventure.c
@@ -1,18 +1,16 @@
 // Copyright 2015 Ian Kronquist.
 #define _GNU_SOURCE
-#include <stdio.h>
-#include <stdbool.h>
-#include <time.h>
-#include <string.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <pwd.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <sys/types.h>
-#include <pwd.h>
-#include <uuid/uuid.h>
 #include <dirent.h>
+#include <math.h>
+#include <pwd.h>
+#include <assert.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+#include <sys/stat.h>
 
 // Define spec'd constants.
 #define MIN_CONN 3
@@ -20,6 +18,20 @@
 #define NUM_ROOMS 7
 #define NUM_ROOM_NAMES 10
 #define NAME_BUFFER_LEN 12
+
+// The list of room names.
+const char *room_names[NUM_ROOM_NAMES] = {
+        "arrakis", // Herbet's Dune
+        "tatooine", // Lucas' Star Wars
+        "winter", // LeGuin's The Left Hand of Darkness
+        "trantor", // Asimov's Foundation
+        "lusitania", // Card's Xenophobia
+        "nasqueron", // Banks' The Algebraist
+        "caprica", // Larson's Battlestart Galactica
+        "camazotz", // L'Engle's A Wrinkle In Time
+        "disk world", // Pratchet's Disk World
+        "magrathea" // Adams' Hitchhiker's Guide to The Galaxy
+};
 
 // The three types of rooms.
 enum room_type {
@@ -36,26 +48,12 @@ struct room {
         unsigned int num_conns;
         struct room *connections[NUM_ROOMS];
 };
-
-// The list of room names.
-const char *room_names[NUM_ROOM_NAMES] = {
-        "arrakis", // Herbet's Dune
-        "tatooine", // Lucas' Star Wars
-        "winter", // LeGuin's The Left Hand of Darkness
-        "trantor", // Asimov's Foundation
-        "lusitania", // Card's Xenophobia
-        "nasqueron", // Banks' The Algebraist
-        "caprica", // Larson's Battlestart Galactica
-        "camazotz", // L'Engle's A Wrinkle In Time
-        "disk world", // Pratchet's Disk World
-        "magrathea" // Adams' Hitchhiker's Guide to The Galaxy
-};
-
 struct room rooms_list[NUM_ROOMS];
 
 void repl();
 
 struct room *generate_rooms();
+void print_room(unsigned int room);
 int create_rooms_dir(char *dir_name);
 void serialize_rooms(struct room rooms[NUM_ROOMS]);
 bool connect(unsigned int room1, unsigned int room2,
@@ -75,11 +73,56 @@ int main() {
         repl();
 }
 
+void ending_message(unsigned int num_steps, struct room **visited,
+                    unsigned int visited_index) {
+
+        printf("YOU HAVE FOUND THE END ROOM. CONGRATULATIONS!\n");
+        printf("YOU TOOK %d STEPS. YOUR PATH TO VICTORY WAS:\n", num_steps);
+        for (int i = 0; i < visited_index; i++) {
+                printf("%s\n", visited[i]->name);
+        }
+}
+
 void repl() {
         struct room *current_room = &rooms_list[0];
+        struct room **visited = malloc(sizeof(struct room*) * NUM_ROOMS);
+        unsigned int visited_index = 0;
+        unsigned int visited_cap = NUM_ROOMS;
+        char buffer[NAME_BUFFER_LEN];
+        unsigned int num_steps = 0;
         while (true) {
-                printf("a");
+                Top:
+                if (current_room->type == END_ROOM) {
+                        ending_message(num_steps, visited, visited_index);
+                        return;
+                }
+
+                printf("\nCURRENT LOCATION: %s\n", current_room->name);
                 print_all_connections(current_room);
+                printf("WHERE TO? >");
+                char *ret = fgets(buffer, NAME_BUFFER_LEN, stdin);
+                assert(ret != NULL);
+                buffer[strlen(buffer)-1] = '\0';
+                for (int i = 0; i < current_room->num_conns; i++) {
+                        if (strncmp(buffer, current_room->connections[i]->name,
+                                    NAME_BUFFER_LEN) == 0) {
+                                current_room = current_room->connections[i];
+
+                                if (visited_index >= visited_cap) {
+                                        visited_cap += NUM_ROOMS *
+                                                       sizeof(struct room*);
+                                        visited = realloc(visited,
+                                                          visited_cap);
+                                        assert(visited != NULL);
+                                }
+                                visited[visited_index] = current_room;
+                                visited_index++;
+                                num_steps++;
+
+                                goto Top;
+                        }
+                }
+                printf("\nHUH? I DON’T UNDERSTAND THAT ROOM. TRY AGAIN.\n");
         }
 }
 
@@ -87,22 +130,28 @@ struct room *generate_rooms() {
         bool taken_names[NUM_ROOMS];
         memset(&taken_names, 0, NUM_ROOMS * sizeof(bool));
         for (int i = 0; i < NUM_ROOMS; i++) {
+                rooms_list[i].num_conns = 0;
+                unsigned int cap_conns = rand() % (MAX_CONN - MIN_CONN);
+                cap_conns += MIN_CONN;
+                rooms_list[i].cap_conns = cap_conns;
                 while (true) {
                         unsigned int room_index = rand() % NUM_ROOMS;
                         if (!taken_names[room_index]) {
                                 taken_names[room_index] = true;
+                                rooms_list[i].name = room_names[room_index];
                                 break;
                         }
-                        rooms_list[i].name = room_names[room_index];
+
                 }
-
                 rooms_list[i].type = MID_ROOM;
+        }
 
-                unsigned int num_conns = rand() % (MAX_CONN - MIN_CONN);
-                num_conns += MIN_CONN;
-                for (int j = 0; j < num_conns; j++) {
+        for (int i = 0; i < NUM_ROOMS; i++) {
+                for (int j = 0; j < rooms_list[i].cap_conns; j++) {
                         unsigned int random_room = rand() % NUM_ROOMS;
-                        while (!connect(i, random_room, rooms_list)) { }
+                        while (!connect(i, random_room, rooms_list)) {
+                                random_room = rand() % NUM_ROOMS;
+                        }
                 }
         }
         rooms_list[0].type = START_ROOM;
@@ -110,51 +159,76 @@ struct room *generate_rooms() {
         return rooms_list;
 }
 
-bool connect(unsigned int room1, unsigned int room2,
-                struct room rooms_list[NUM_ROOMS]) {
-        if (rooms_list[room1].num_conns >= MAX_CONN ||
-                        rooms_list[room2].num_conns >= MAX_CONN) {
-                puts("1");
-                exit(1);
-                STUCK HERE
-                look at cap conns
-                return false;
+bool already_connected(unsigned int room1, unsigned int room2) {
+        if (room1 == room2) {
+                return true;
         }
         for (int i = 0; i < rooms_list[room1].num_conns; i++) {
-                if (rooms_list[room1].connections[i] == &rooms_list[room2]) {
-                        puts("2");
-                        exit(2);
-                        return false;
+                if (rooms_list[room1].connections[i] == &rooms_list[room2] &&
+                    rooms_list[room1].connections[i] != NULL) {
+                        return true;
                 }
         }
-        int r1cons = rooms_list[room1].num_conns;
-        int r2cons = rooms_list[room2].num_conns;
-        rooms_list[room1].connections[r1cons-1] = &rooms_list[room2];
-        rooms_list[room2].connections[r2cons-1] = &rooms_list[room1];
+        return false;
+}
+
+bool connect(unsigned int room1, unsigned int room2,
+                struct room rooms_list[NUM_ROOMS]) {
+        struct room *r1 = &rooms_list[room1];
+        struct room *r2 = &rooms_list[room2];
+        if (r1->num_conns == MAX_CONN) {
+                return true;
+        }
+        if (already_connected(room1, room2)) {
+                return false;
+        }
+        if (r1->num_conns >= MAX_CONN || r2->num_conns >= MAX_CONN) {
+                return false;
+        }
+        assert(r1 != NULL);
+        assert(r2 != NULL);
+        r1->connections[r1->num_conns] = r2;
+        r2->connections[r2->num_conns] = r1;
+        r1->num_conns++;
+        r2->num_conns++;
+        assert(r1->connections[r1->num_conns-1] != NULL);
+        assert(r2->connections[r2->num_conns-1] != NULL);
         return true;
 }
 
-char **get_dir_name() {
+void print_room(unsigned int room) {
+        printf("name: %s,\nnum_conns %d,\ncap_conns %d,\n",
+                rooms_list[room].name,
+                rooms_list[room].num_conns,
+                rooms_list[room].cap_conns
+        );
+        for (int i = 0; i < rooms_list[room].num_conns; i++) {
+                printf("connection: %s\n", rooms_list[room].connections[i]->name);
+        }
+}
+
+char *get_dir_name() {
         pid_t pid = getpid();
 
         uid_t uid = getuid();
         struct passwd *user_info = getpwuid(uid);
-        char **dir_name;
-        asprintf(dir_name, "%s.rooms.%d", user_info->pw_name, pid);
-        free(user_info);
+        unsigned int buffer_max_len = strlen(".rooms.") +
+                                      strlen(user_info->pw_name) + log10(pid);
+        char *dir_name = malloc(buffer_max_len * sizeof(char));
+        assert(dir_name != NULL);
+        sprintf(dir_name, "%s.rooms.%d", user_info->pw_name, pid);
+        //free(user_info);
         return dir_name;
-
 }
 
 void serialize_rooms(struct room rooms[NUM_ROOMS]) {
-        char **dir_name = get_dir_name();
-        mkdir(*dir_name, 0777);
-        chdir(*dir_name);
+        char *dir_name = get_dir_name();
+        mkdir(dir_name, 0777);
+        chdir(dir_name);
         for (int i = 0; i < NUM_ROOMS; i++) {
                 FILE *fp = fopen(rooms[i].name, "w");
                 fprintf(fp, "ROOM NAME: %s\n", rooms[i].name);
 
-                char **connection_name;
                 for (int j = 0; j < rooms[i].num_conns; j++) {
                         fprintf(fp, "CONNECTION %d: %s\n", j + 1, rooms[j].name);
                 }
@@ -173,7 +247,7 @@ void serialize_rooms(struct room rooms[NUM_ROOMS]) {
                 fclose(fp);
         }
         chdir("..");
-        free(*dir_name);
+        free(dir_name);
 }
 
 const char *pick_right_name(char *in) {
@@ -200,12 +274,14 @@ void iter_rooms(struct room *r, void (*func)(struct room *)) {
         }
 }
 
-void print_all_connections_helper(struct room *r) {
-        printf("%s\n", r->name);
-}
-
 void print_all_connections(struct room *r) {
-        iter_rooms(r, &print_all_connections_helper);
+        printf("POSSIBLE CONNECTIONS: ");
+        for (int i = 0; i < r->num_conns-1; i++) {
+                printf("%s, ", r->connections[i]->name);
+        }
+        if (r->num_conns > 0) {
+                printf("%s.\n", r->connections[r->num_conns-1]->name);
+        }
 }
 
 struct room deserialize_single_room(char *name) {
@@ -243,9 +319,9 @@ struct room deserialize_single_room(char *name) {
 struct room* deserialize_rooms() {
         struct room *rooms = malloc(NUM_ROOMS * sizeof(struct room));
         unsigned int room_count = 0;
-        char **dir_name = get_dir_name();
+        char *dir_name = get_dir_name();
         // FIXME: Make sure that directory exists with stat
-        chdir(*dir_name);
+        chdir(dir_name);
         DIR *dp;
         struct dirent *dir;
         dp = opendir (".");
@@ -262,7 +338,7 @@ struct room* deserialize_rooms() {
                 puts("test");
         }
 
-        free(*dir_name);
+        free(dir_name);
         chdir("..");
         return rooms;
 }
